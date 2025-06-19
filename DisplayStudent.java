13fa4f3d5e09a8f3f62173237646f93e2ea35770
Md. Sabbir Ahmed <148763546+SabbirAhad05@users.noreply.github.com>
2025-06-19 23:17:34 +0600

    Update DisplayStudent.java

--- DisplayStudent.java
+++ DisplayStudent.java
@@ -1,5 +1,6 @@
 package project207;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DisplayStudent {
@@ -7,6 +8,10 @@ public class DisplayStudent {
 	public static void main(String[] args){
 		AlgorithmsOfProject aop=new AlgorithmsOfProject();
 		Scanner input=new Scanner(System.in);
+		 boolean continueInput = true;
+		 do {
+			 try {
+		
 		boolean flag=true;
 		while(flag) {
 			System.out.println("Press 1: To Insert");
@@ -23,8 +28,14 @@ public class DisplayStudent {
 				if(aop.start==null) {
 					System.out.println("Record is Empty");
 				}else {
-				System.out.println("All Students Information in ascending order: ");
+				System.out.println("All Students Information in InOrder: ");
 				aop.printAll(aop.start);
+				System.out.println(" ");
+				System.out.println("All Students Information in PreOrder: ");
+				aop.printPREorder(aop.start);
+				System.out.println(" ");
+				System.out.println("All Students Information in PostOrder: ");
+				aop.printPostorder(aop.start);
 			}
 			}
              else if(userInput==3) {
@@ -47,14 +58,15 @@ public class DisplayStudent {
             	 aop.printInorder(aop.start);
             	 System.out.println("\nGive the roll no. want to delete: ");
          		int b=input.nextInt();
-       
+     
          		aop.delete(b);
          		if(aop.delete(b)==0) {
          	    System.out.println("Record Deleted Successfully");
+         		}else {
+         			System.out.println("Wrong roll");
          		}
-         		else {
-         			System.out.println("List now Empty!!");
-         		}
+         		
+         		
          		}
 
  			}
@@ -62,8 +74,15 @@ public class DisplayStudent {
  				flag=false;
  			}
 		}
-	
+		 
+		 catch(InputMismatchException ex) {
+			 System.out.println("Try again...Incorrect input");
+			 input.nextLine();
+		 }
+		 
+	 }
+	 while (continueInput);
 		
 	}
-
+}
 
